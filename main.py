88b9b6321e256b59cf5d8f4f41a2d6884f707fcf
Lechoslaw Radwanski <lupercaldev@gmail.com>
2021-01-15 02:30:23 +0100

    fix constructor

--- main.py
+++ main.py
@@ -3,23 +3,6 @@ class BankingSystem:
     def __init__(self):
         self.accounts = {}
 
-    def old_run(self):
-        while True:
-            self._print_main_menu()
-            option = int(input())
-            if option == 1:
-                acc = self.create_account()
-                print("Your card number:")
-                print(acc.card_number)
-                print("Your card PIN:")
-                print(acc.pin)
-            elif option == 2:
-                card_number = input("Enter your card number:")
-                card_pin = input("Enter your PIN:")
-                self.handle_login(card_number, card_pin)
-            else:
-                break
-
     def create_account(self):
         acc = Account()
         self.accounts += {acc.card_number, acc}
@@ -42,12 +25,6 @@ class BankingSystem:
             print("Wrong card number or PIN!")
             return MainMenu()
 
-    def _print_main_menu(self):
-        print("""1. Create an account
-    2. Log into account
-    0. Exit
-    """)
-
 
 class Account:
 
@@ -62,7 +39,7 @@ class MainMenu:
     option2 = "2. Log into account"
     option0 = "0. Exit"
 
-    def __init_(self, bs: BankingSystem):
+    def __init__(self, bs: BankingSystem):
         self.bs = bs
 
     def wait_for_input(self):
